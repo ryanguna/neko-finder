54f2b3c7c5c5f681d7c137f567ed1cd52fa13ecf
ryanguna <ryan.g@heyyou.com.au>
2022-02-10 05:39:34 +0800

    Updated UiButton component

--- src/components/UiButton/index.tsx
+++ src/components/UiButton/index.tsx
@@ -1,21 +1,16 @@
 /**
  * Module Dependencies
  */
-import '@screens/app/style.scss';
+import '@components/UiButton/style.scss';
 
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 
 function UiButton(props: any) {
+  const { sharp, description, ...other } = props;
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={props.img} />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text>{props.description}</Card.Text>
-        <Button variant="primary">{props.buttonDescription}</Button>
-      </Card.Body>
-    </Card>
+    <Button className={sharp ? 'btn-sharp' : ''} {...other}>
+      {props.children}
+    </Button>
   );
 }
 
